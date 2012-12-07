e1233032cf607566e5bbdbe214670125075ce102
Yutaro Sugai <y.sugai@everyleaf.com>
2012-12-07 10:32:14 +0900

    Add postgresql84

---
+++ postgresql84/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: postgresql84
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "postgresql-8.4" do
+  action :install
+end
