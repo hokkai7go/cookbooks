a66904200f70ab2ef772aac6e8ee11a5db4cebf1
Yutaro Sugai <y.sugai@everyleaf.com>
2012-12-07 10:55:13 +0900

    Support to networking slowness http://vagrantup.com/v1/docs/troubleshooting.html

---
+++ libavahi-common3/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: libavahi-common3
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "libavahi-common3" do
+  action :remove
+end
