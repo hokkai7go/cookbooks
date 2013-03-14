d67402343fac04507bcf1a4b069f45cbc737713a
Yutaro Sugai <y.sugai@everyleaf.com>
2013-03-15 01:46:48 +0900

    Add coreutils for zsh.d

---
+++ coreutils/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: coreutils
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "coreutils" do
+  action :install
+end
