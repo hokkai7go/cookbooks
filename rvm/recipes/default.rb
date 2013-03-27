9e1fe59ad28ccc9cf29121ea722006b532810089
Yutaro Sugai <y.sugai@everyleaf.com>
2013-03-27 10:19:57 +0900

    Add libs

--- rvm/recipes/default.rb
+++ rvm/recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright 2012, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-packages = %w{build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config imagemagick libmagick++-dev}
+packages = %w{build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config imagemagick libmagick++-dev libgdbm-dev libffi-dev}
 
 packages.each do |package_name|
   package package_name do
