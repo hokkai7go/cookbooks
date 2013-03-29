b0ab250d7be6b80a925802e1884518b3090825c7
Yutaro Sugai <y.sugai@everyleaf.com>
2013-03-29 19:46:54 +0900

    libpq-dev need when install pg

---
+++ libpq-dev/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: libpq-dev
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+
+package "libpq-dev" do
+  action :install
+end
